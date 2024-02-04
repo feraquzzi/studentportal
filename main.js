fd90ea6c30eaba970963af7207337aeb4e361241
feraquzzi <mosesobembe@gmail.com>
2024-02-04 17:27:36 +0100

    worked on the functionalities

--- main.js
+++ main.js
@@ -1,45 +1,4 @@
-// array of objects to be queried
-const array = [
-  {
-    id: 1,
-    image: "https://i.postimg.cc/fWHQQbrT/omolola-adio.jpg",
-    name: 'Shubham',
-    weeklyAttendance: 2,
-    softSkillAttendance: 2,
-    test: 10,
-    softSkillProject: 12,
-    finalProject: 32,
-    allScores: 56
 
-  },
-
-  {
-    id: 2,
-    image: "https://i.postimg.cc/fWHQQbrT/omolola-adio.jpg",
-    name: 'Shubham',
-    weeklyAttendance: 2,
-    softSkillAttendance: 2,
-    test: 10,
-    softSkillProject: 12,
-    finalProject: 32,
-    allScores: 56
-  },
-
-  {
-    id: 3,
-    image: "https://i.postimg.cc/fT7W5Jff/oluwapelumi-obatola.jpg",
-    name: 'Shubham',
-    weeklyAttendance: 2,
-    softSkillAttendance: 2,
-    test: 10,
-    softSkillProject: 12,
-    finalProject: 32,
-    allScores: 56
-  }
-]
-  
-  
-  
 function checkName() {
     const studentId = document.getElementById('demo');
   
