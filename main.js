5c1bc118b05e92b5bf991d8a054f20571b25749c
feraquzzi <mosesobembe@gmail.com>
2024-02-04 17:07:24 +0100

    worked on the functionalities

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
 // array of objects to be queried
 const array = [
-    {
-      id: 1,
-      image: "https://i.postimg.cc/fWHQQbrT/omolola-adio.jpg",
-      name: 'Shubham',
-      age: 16
-    },
+  {
+    id: 1,
+    image: "https://i.postimg.cc/fWHQQbrT/omolola-adio.jpg",
+    name: 'Shubham',
+    weeklyAttendance: 2,
+    softSkillAttendance: 2,
+    test: 10,
+    softSkillProject: 12,
+    finalProject: 32,
+    allScores: 56
 
-    {
-      id: 2,
-      image: "https://i.postimg.cc/prHsdCrN/morufat-lamidi.jpg",
-      name: 'Parth',
-      age: 18
-    },
+  },
 
-    {
-      id: 3,
-      image: "https://i.postimg.cc/fT7W5Jff/oluwapelumi-obatola.jpg",
-      name: 'Pratik',
-      age: 21
-    }
+  {
+    id: 2,
+    image: "https://i.postimg.cc/fWHQQbrT/omolola-adio.jpg",
+    name: 'Shubham',
+    weeklyAttendance: 2,
+    softSkillAttendance: 2,
+    test: 10,
+    softSkillProject: 12,
+    finalProject: 32,
+    allScores: 56
+  },
+
+  {
+    id: 3,
+    image: "https://i.postimg.cc/fT7W5Jff/oluwapelumi-obatola.jpg",
+    name: 'Shubham',
+    weeklyAttendance: 2,
+    softSkillAttendance: 2,
+    test: 10,
+    softSkillProject: 12,
+    finalProject: 32,
+    allScores: 56
+  }
 ]
   
   
@@ -39,6 +55,12 @@ function checkName() {
    if (object) {
         //document.getElementById('container').innerHTML = object.name;
         document.getElementById('photo').src = object.image;
+        document.getElementById('weeklyAttendance').innerHTML = object.weeklyAttendance;
+        document.getElementById('softSkillAttendance').innerHTML = object.softSkillAttendance;
+        document.getElementById('test').innerHTML = object.test;
+        document.getElementById('softSkillProject').innerHTML = object.softSkillProject;
+        document.getElementById('finalProject').innerHTML = object.finalProject;
+        document.getElementById('totalScore').innerHTML = object.allScores;
    } else {
         document.getElementById('error').innerHTML = 'This ID is not valid or Your ID is incorrect';
    }
