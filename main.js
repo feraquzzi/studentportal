2567d152e34a2063eff7b923f73d914d71849ad5
feraquzzi <mosesobembe@gmail.com>
2024-02-04 14:56:06 +0100

    design the look and feel of the website

---
+++ main.js
@@ -0,0 +1,49 @@
+// array of objects to be queried
+const array = [
+    {
+      id: 1,
+      avatar: "https://i.postimg.cc/fWHQQbrT/omolola-adio.jpg",
+      name: 'Shubham',
+      age: 16
+    },
+
+    {
+      id: 2,
+      avatar: "https://i.postimg.cc/prHsdCrN/morufat-lamidi.jpg",
+      name: 'Parth',
+      age: 18
+    },
+
+    {
+      id: 3,
+      avatar: "https://i.postimg.cc/fT7W5Jff/oluwapelumi-obatola.jpg",
+      name: 'Pratik',
+      age: 21
+    }
+]
+  
+  
+  
+function checkName() {
+    const studentId = document.getElementById('demo');
+  
+   const studentValue = studentId.value;
+   const studId = Math.ceil(studentValue);
+    
+    
+    // finding the object whose id is '3'
+
+    
+   const object = array.find(obj => obj.id === studId);
+
+   if (object) {
+        document.getElementById('container').innerHTML = object.name;
+        document.getElementById('studentPhoto').src = object.avatar;
+   } else {
+        document.getElementById('error').innerHTML = 'This ID is not valid or Your ID is incorrect';
+   }
+  
+  // printing object on the console
+    console.log(object.name);
+
+}
